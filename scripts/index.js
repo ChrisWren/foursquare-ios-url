57b9f2a46df4f013a14a7ae175672032d92b2859
ChrisWren <cthewren@gmail.com>
2014-11-05 19:13:05 +0100

    Updates

---
+++ scripts/index.js
@@ -0,0 +1,8 @@
+$('#foursquareVenueURL').on('paste', function() {
+  setTimeout(function() {
+    if ($('#foursquareVenueURL').val()) {
+      console.log('foursquare://venues/' + _.last($('#foursquareVenueURL').val().split('/')));
+      window.location = 'foursquare://venues/' + _.last($('#foursquareVenueURL').val().split('/'))
+    }
+  }, 0);
+})
